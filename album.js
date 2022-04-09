e81f0e2f29568271803557311a2e82ebec310886
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-04-09 16:22:10 +0300

    updated

--- album.js
+++ album.js
@@ -31,7 +31,6 @@ const loadAlbumPage = () => {
       const bigImg = document.querySelector(`.big-img`);
       const btNote = document.querySelector(`.bt-note`);
       const btText = document.querySelector(`.bt-text`);
-      console.log(data.genre_id);
       btText.innerHTML = `<a href="artist.html?id=${data.artist.id}"> ${data.artist.name}</a>`;
       const slicedDate = `${data.release_date}`.slice(0, 4);
       btNote.innerText = `${slicedDate}•${data.tracks.data.length}songs`;
@@ -46,7 +45,7 @@ const loadAlbumPage = () => {
         let songDuration = (track.duration / 60).toFixed(2);
         let songDurationMain = String(songDuration).split(`.`).join(`:`);
 
-        const newRow = ` <tr id=${i + 1} class="songs">
+        const newRow = ` <tr id=${i} class="songs">
         <th scope="row">
           <span class=""
             >${i + 1}</span>
@@ -69,7 +68,6 @@ const loadAlbumPage = () => {
     .catch((err) => console.error(err));
 };
 
-let songIndex = 0;
 // Update song details
 const load = () => {
   document.querySelectorAll(`.songs`).forEach((song) => {
@@ -77,16 +75,16 @@ const load = () => {
       // console.log(e.target);
       songIndex = parseInt(this.getAttribute(`id`));
       console.log(songIndex);
-      audio.src = "" + songsPreview[songIndex];
+      audio.src = songsPreview[songIndex];
       audio.play();
       // loadSong(songsPreview[songIndex]);
-      // tbody.querySelectorAll(`tr`).forEach((tr) => {
-      //   if (tr.classList.contains(`text-success`)) {
-      //     tr.classList.remove(`text-success`);
-      //   }
-      // });
-      // song.classList.add(`text-success`);
-      // playSong();
+      tbody.querySelectorAll(`tr`).forEach((tr) => {
+        if (tr.classList.contains(`text-success`)) {
+          tr.classList.remove(`text-success`);
+        }
+      });
+      song.classList.add(`text-success`);
+      playSong();
     });
   });
 };
@@ -167,3 +165,157 @@ audio.addEventListener(`timeupdate`, updateProgress);
 prev.addEventListener(`click`, prevSong);
 next.addEventListener(`click`, nextSong);
 audio.addEventListener(`ended`, nextSong);
+/* play section
+const WizkidMusicTitles = document.querySelectorAll(`tbody tr figure + span`);
+const tbody = document.querySelector(`tbody`);
+const next = document.querySelector(`.next`);
+const play = document.querySelector(`.play`);
+const prev = document.querySelector(`.prev`);
+const progressContainer = document.querySelector(`.progress-container`);
+const audio = document.querySelector(`audio`);
+const musicTitle = document.querySelector(`.music-title`);
+const progress = document.querySelector(`.progress`);
+const cover = document.querySelector(`.playing-img`);
+const musicContainer = document.querySelector(`.music-container`);
+const bigPlayBtn = document.querySelector(`.artist-page-play`);
+const bigPauseBtn = document.querySelector(`.pause-btn`);
+// Song Titles
+const songs = [
+  "Holla At Your Boy",
+  "Don't Dull",
+  "Eme-Boyz-ft-Skales-Banky-W",
+  "For Me ft Wande-Coal",
+  "Gidi Girl",
+  "Love My Baby",
+  "No lele",
+  "Oluwa Lo Ni",
+  "Pakurumo",
+  "Say My Name",
+  "Scatter The Floor",
+  "Shout Out",
+  "Slow Whine",
+  "Tease Me Bad Guys",
+  "Wad-Up ft D-prince",
+  "What you wanna do",
+  "Wiz Party Bonus Freestyle Leak",
+];
+// creating wizkid's 17 music album
+// for (let i = 0; i < 17; i++) {
+//   tbody.innerHTML += ` <tr class="song" id="${i}">
+//   <th scope="row">${i + 1}</th>
+//   <td>
+//     <div class="d-flex gap-2 align-items-center">
+//       <figure>
+//         <img
+//           class="img-fluid wiz-img"
+//           src="./img/Wizkid images/wizkid ${i + 1}.jpg"
+//           alt=""
+//         />
+//       </figure>
+//       <span>${songs[i]}</span>
+//     </div>
+//   </td>
+//   <td>${randomIntFromInterval(250000 * i, 900000)}</td>
+//   <td class="duration">3:34</td>
+// </tr>`;
+// }
+// generate random number
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+let songIndex = 0;
+document.querySelectorAll(`.song`).forEach((song) => {
+  song.addEventListener(`click`, function (e) {
+    songIndex = parseInt(this.getAttribute(`id`));
+    // console.log(songIndex);
+    loadSong(songs[songIndex]);
+    tbody.querySelectorAll(`tr`).forEach((tr) => {
+      if (tr.classList.contains(`text-success`)) {
+        tr.classList.remove(`text-success`);
+      }
+    });
+    song.classList.add(`text-success`);
+    playSong();
+  });
+});
+// Update song details
+const loadSong = function (song) {
+  musicTitle.innerText = song;
+  audio.src = `./Superstar Album/${song}.mp3`;
+  cover.src = `./img/Wizkid images/wizkid ${songIndex + 1}.jpg`;
+};
+const playSong = function () {
+  musicContainer.classList.add(`play`);
+  play.querySelector(`i.bi`).classList.remove(`bi-play-circle`);
+  play.querySelector(`i.bi`).classList.add(`bi-pause-circle`);
+  bigPlayBtn.classList.remove(`play-btn`);
+  bigPlayBtn.classList.add(`pause-btn`);
+  audio.play();
+};
+const pauseSong = function () {
+  musicContainer.classList.remove(`play`);
+  play.querySelector(`i.bi`).classList.add(`bi-play-circle`);
+  play.querySelector(`i.bi`).classList.remove(`bi-pause-circle`);
+  bigPlayBtn.classList.add(`play-btn`);
+  bigPlayBtn.classList.remove(`pause-btn`);
+  audio.pause();
+};
+const prevSong = function () {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+const nextSong = function () {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+const updateProgress = function (e) {
+  const movingMinutes = Math.floor(e.srcElement.currentTime);
+  const totalMinutes = Math.floor(e.srcElement.duration);
+  let songDuration = (totalMinutes / 60).toFixed(2);
+  let songDurationMain = String(songDuration).split(`.`).join(`:`);
+  document.querySelector(`.minutes-end`).textContent = `${
+    isNaN(songDuration) ? ` ` : songDurationMain
+  }`;
+  const progressPercent = (movingMinutes / totalMinutes) * 100;
+  progress.style.width = `${progressPercent}%`;
+};
+const setProgress = function (e) {
+  const width = this.clientWidth;
+  // console.log(width);
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+  console.log(audio.currentTime);
+};
+// Initially load song info DOM
+loadSong(songs[songIndex]);
+
+// event listeners
+play.addEventListener(`click`, function () {
+  const isPlaying = musicContainer.classList.contains(`play`);
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// change song events
+progressContainer.addEventListener(`click`, setProgress);
+audio.addEventListener(`timeupdate`, updateProgress);
+prev.addEventListener(`click`, prevSong);
+next.addEventListener(`click`, nextSong);
+audio.addEventListener(`ended`, nextSong);
+// bigPlayBtn.addEventListener(`click`, playSong);
+// bigPauseBtn.addEventListener(`click`, pauseSong);*/
--- script.js
+++ script.js
@@ -46,9 +46,6 @@ let artistName = [
   "rihanna",
 ];
 let randArtistName = artistName[Math.floor(Math.random() * artistName.length)];
-window.onload = () => {
-  loadArtistAlbum(`${randArtistName}`);
-};
 
 const artistPage = document.querySelector(`.artist-page`);
 const albumPage = document.querySelector(`.album-page`);
@@ -148,14 +145,12 @@ const checkCurrentTime = function () {
 const init = function () {
   HometoDarkMode();
   HometoLightMode();
-  artistPageToDarkMode();
-  artistPageTolightMode();
+
+  loadArtistAlbum(`${randArtistName}`);
+
   randomBackgroundColor();
-  goToArtistPage();
-  goToHomePage();
-  goToArtistPage2();
-  goToAlbumPage();
-  aside.classList.remove(`d-none`);
+
+  // aside.classList.remove(`d-none`);
 };
 logInBtn.addEventListener(`click`, function (e) {
   if (password.value === `12345`) {
@@ -189,158 +184,3 @@ logInBtn.addEventListener(`click`, function (e) {
     }, 4500);
   }
 });
-
-//* play section
-const WizkidMusicTitles = document.querySelectorAll(`tbody tr figure + span`);
-const tbody = document.querySelector(`tbody`);
-const next = document.querySelector(`.next`);
-const play = document.querySelector(`.play`);
-const prev = document.querySelector(`.prev`);
-const progressContainer = document.querySelector(`.progress-container`);
-const audio = document.querySelector(`audio`);
-const musicTitle = document.querySelector(`.music-title`);
-const progress = document.querySelector(`.progress`);
-const cover = document.querySelector(`.playing-img`);
-const musicContainer = document.querySelector(`.music-container`);
-const bigPlayBtn = document.querySelector(`.artist-page-play`);
-const bigPauseBtn = document.querySelector(`.pause-btn`);
-// Song Titles
-const songs = [
-  "Holla At Your Boy",
-  "Don't Dull",
-  "Eme-Boyz-ft-Skales-Banky-W",
-  "For Me ft Wande-Coal",
-  "Gidi Girl",
-  "Love My Baby",
-  "No lele",
-  "Oluwa Lo Ni",
-  "Pakurumo",
-  "Say My Name",
-  "Scatter The Floor",
-  "Shout Out",
-  "Slow Whine",
-  "Tease Me Bad Guys",
-  "Wad-Up ft D-prince",
-  "What you wanna do",
-  "Wiz Party Bonus Freestyle Leak",
-];
-// creating wizkid's 17 music album
-// for (let i = 0; i < 17; i++) {
-//   tbody.innerHTML += ` <tr class="song" id="${i}">
-//   <th scope="row">${i + 1}</th>
-//   <td>
-//     <div class="d-flex gap-2 align-items-center">
-//       <figure>
-//         <img
-//           class="img-fluid wiz-img"
-//           src="./img/Wizkid images/wizkid ${i + 1}.jpg"
-//           alt=""
-//         />
-//       </figure>
-//       <span>${songs[i]}</span>
-//     </div>
-//   </td>
-//   <td>${randomIntFromInterval(250000 * i, 900000)}</td>
-//   <td class="duration">3:34</td>
-// </tr>`;
-// }
-// generate random number
-function randomIntFromInterval(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-let songIndex = 0;
-document.querySelectorAll(`.song`).forEach((song) => {
-  song.addEventListener(`click`, function (e) {
-    songIndex = parseInt(this.getAttribute(`id`));
-    // console.log(songIndex);
-    loadSong(songs[songIndex]);
-    tbody.querySelectorAll(`tr`).forEach((tr) => {
-      if (tr.classList.contains(`text-success`)) {
-        tr.classList.remove(`text-success`);
-      }
-    });
-    song.classList.add(`text-success`);
-    playSong();
-  });
-});
-// Update song details
-const loadSong = function (song) {
-  musicTitle.innerText = song;
-  audio.src = `./Superstar Album/${song}.mp3`;
-  cover.src = `./img/Wizkid images/wizkid ${songIndex + 1}.jpg`;
-};
-const playSong = function () {
-  musicContainer.classList.add(`play`);
-  play.querySelector(`i.bi`).classList.remove(`bi-play-circle`);
-  play.querySelector(`i.bi`).classList.add(`bi-pause-circle`);
-  bigPlayBtn.classList.remove(`play-btn`);
-  bigPlayBtn.classList.add(`pause-btn`);
-  audio.play();
-};
-const pauseSong = function () {
-  musicContainer.classList.remove(`play`);
-  play.querySelector(`i.bi`).classList.add(`bi-play-circle`);
-  play.querySelector(`i.bi`).classList.remove(`bi-pause-circle`);
-  bigPlayBtn.classList.add(`play-btn`);
-  bigPlayBtn.classList.remove(`pause-btn`);
-  audio.pause();
-};
-const prevSong = function () {
-  songIndex--;
-  if (songIndex < 0) {
-    songIndex = songs.length - 1;
-  }
-  loadSong(songs[songIndex]);
-  playSong();
-};
-const nextSong = function () {
-  songIndex++;
-  if (songIndex > songs.length - 1) {
-    songIndex = 0;
-  }
-  loadSong(songs[songIndex]);
-  playSong();
-};
-
-const updateProgress = function (e) {
-  const movingMinutes = Math.floor(e.srcElement.currentTime);
-  const totalMinutes = Math.floor(e.srcElement.duration);
-  let songDuration = (totalMinutes / 60).toFixed(2);
-  let songDurationMain = String(songDuration).split(`.`).join(`:`);
-  document.querySelector(`.minutes-end`).textContent = `${
-    isNaN(songDuration) ? ` ` : songDurationMain
-  }`;
-  const progressPercent = (movingMinutes / totalMinutes) * 100;
-  progress.style.width = `${progressPercent}%`;
-};
-const setProgress = function (e) {
-  const width = this.clientWidth;
-  // console.log(width);
-  const clickX = e.offsetX;
-  const duration = audio.duration;
-
-  audio.currentTime = (clickX / width) * duration;
-  console.log(audio.currentTime);
-};
-// Initially load song info DOM
-loadSong(songs[songIndex]);
-
-// event listeners
-play.addEventListener(`click`, function () {
-  const isPlaying = musicContainer.classList.contains(`play`);
-  if (isPlaying) {
-    pauseSong();
-  } else {
-    playSong();
-  }
-});
-
-// change song events
-progressContainer.addEventListener(`click`, setProgress);
-audio.addEventListener(`timeupdate`, updateProgress);
-prev.addEventListener(`click`, prevSong);
-next.addEventListener(`click`, nextSong);
-audio.addEventListener(`ended`, nextSong);
-// bigPlayBtn.addEventListener(`click`, playSong);
-// bigPauseBtn.addEventListener(`click`, pauseSong);
